dd65fafab4520b1e74dc49f5a0deea809bddf771
Kristóf Nyári <nyari.kristof66@gmail.com>
2023-05-25 13:22:29 +0200

    feat(Lambda): :sparkles: Created Lambda function with Hello handler

--- lib/aws-cdk-ts-stack.ts
+++ lib/aws-cdk-ts-stack.ts
@@ -1,19 +1,18 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
 
 export class AwsCdkTsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsCdkTsQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      //Directory from which lambda should run the code
+      code: lambda.Code.fromAsset('lambda'),
+      //<filename.functionName> callback to run
+      handler: 'hello.handler'
+    })
 
-    const topic = new sns.Topic(this, 'AwsCdkTsTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
